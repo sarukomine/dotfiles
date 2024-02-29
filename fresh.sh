be2e98b895da6caa62741e339bf9805d25e6fc1f
Alvin Lau <sarukomine@gmail.com>
2024-02-29 21:14:55 +0800

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -26,18 +26,6 @@ ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 /opt/homebrew/bin/brew tap homebrew/bundle
 /opt/homebrew/bin/brew bundle --file $HOME/.dotfiles/Brewfile
 
-# Install global NPM packages
-/opt/homebrew/bin/npm install --global pure-prompt
-
-# Install global Composer packages
-/opt/homebrew/bin/composer global require laravel/installer laravel/valet
-
-# Install Laravel Valet
-$HOME/.composer/vendor/bin/valet install
-
-# Create a Projects directory
-mkdir -p $HOME/Projects
-
 # Set macOS preferences - we will run this last because this will reload the shell
 source $HOME/.dotfiles/.macos
 
